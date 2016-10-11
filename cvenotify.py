c3f276680835d9a9a35140d5dd34bf92bdd6a7e7
Kai Renken <git-kai@koffeinucht.de>
2016-10-11 12:44:26 +0200

    send mail for notification

--- cvenotify.py
+++ cvenotify.py
@@ -1,24 +1,43 @@
 # cvenotify
 #
-# Kai Renken (kai@koffeinsucht.de) 
+# Kai Renken (code@koffeinsucht.de) 
 #
 
 from ares import CVESearch
 from pymongo import MongoClient
 import json
+from smtplib import SMTP
+import time
 
 cve = CVESearch('http://localhost:5000/api')
 client = MongoClient('localhost',27017)
 database=client.vulns
 mycollection=database.programs
 
+def notifiy():
+   to = 'foo@foo.bar'
+   fro = 'cve-search@foo.bar'
+   server = SMTP('adress',587)
+   server.set_debuglevel(1)
+   server.ehlo()
+   server.starttls()
+   server.ehlo()
+   server.login('user','pass')
+
+   header = 'To:' + to + '\n' + 'From: ' + fro + '\n' + 'Subject:[CVE-Search] New Vulnerability ' + i['id']+'\n'
+   msg = header + '\nhttp://localhost/cve/'+ i['id'] +'\n\n'
+   server.sendmail(fro, to , msg.format(header))
+   server.quit()
+   #time.sleep(10) #needed if the MessageRateLimit is to small
+   return
+ 
 for entry in mycollection.find():
     queryresult = json.loads(cve.search(entry["vendor"]+'/'+entry["product"]+':'+entry["version"]).decode('utf-8'))
-    
+ 
     # search all new cve's
     for i in queryresult:
        if (i['id'] != entry['lastcve']):
-          print(i['id']) #TODO create bugticket
+          notify()  
        else:
           break;
     
